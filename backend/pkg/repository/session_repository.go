fd8022c61ca4f35e02c45dd5a74c10e0312a29ef
oerraoui <itsscarl2022@gmail.com>
2025-10-16 13:35:43 +0100

    implement CreateSession function for session management

--- backend/pkg/repository/session_repository.go
+++ backend/pkg/repository/session_repository.go
@@ -1,3 +1,24 @@
 package repository
 
-// SessionRepository handles session database operations
+import (
+	"database/sql"
+	// "time"
+	"github.com/asoudri/social-network/pkg/models"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func CreateSession(session models.Session) error {
+	db, err := sql.Open("sqlite3", "./social-network.db")
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	stmt, err := db.Prepare("INSERT INTO sessions(user_id, token, expires_at) VALUES(?, ?, ?)")
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(session.UserID, session.Token, session.ExpiresAt)
+	return err
+}
