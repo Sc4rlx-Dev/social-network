690fae55eaf8838000ae86a74bff42592a5b4a6a
oerraoui <itsscarl2022@gmail.com>
2025-10-16 13:38:41 +0100

    refactor User struct to improve clarity and maintainability

--- backend/pkg/models/user.go
+++ backend/pkg/models/user.go
@@ -1,3 +1,15 @@
 package models
 
-// User represents a user in the system
+import "time"
+
+type User struct {
+	ID           int       `json:"id"`
+	Email        string    `json:"email"`
+	Nickname     string    `json:"nickname"`
+	FirstName    string    `json:"first_name"`
+	LastName     string    `json:"last_name"`
+	PasswordHash string    `json:"-"` // Omit from JSON responses
+	DateOfBirth  string    `json:"date_of_birth"`
+	Gender       string    `json:"gender"`
+	CreatedAt    time.Time `json:"created_at"`
+}
