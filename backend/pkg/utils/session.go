879c87ac7d404ef740d30cd96ba1bf21d53ade77
oerraoui <itsscarl2022@gmail.com>
2025-10-16 13:37:59 +0100

    implement GenerateSessionToken function for creating unique session tokens

--- backend/pkg/utils/session.go
+++ backend/pkg/utils/session.go
@@ -1,3 +1,15 @@
 package utils
 
-// GenerateSessionToken generates a unique session token
+import (
+	"crypto/rand"
+	"encoding/base64"
+)
+
+func GenerateSessionToken() (string, error) {
+	b := make([]byte, 32)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+	return base64.URLEncoding.EncodeToString(b), nil
+}
